d0aaaf243a1369bf1d128668eab51c0fe8c4421b
alonsocanov <alonsocanov@hotmail.com>
2020-12-10 23:45:04 -0600

    changed detect function and set it in utils

--- detect.py
+++ detect.py
@@ -1,144 +1,37 @@
-from torchvision import transforms
-from utils import *
-from PIL import Image, ImageDraw, ImageFont
+from utils import show, labelMap, detect
+from PIL import Image
 import torch
-# from cards_class import showBatch
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Load model checkpoint
-checkpoint = 'data/models/cards.pth.tar'
-checkpoint = torch.load(checkpoint)
-start_epoch = checkpoint['epoch'] + 1
-print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
-model = checkpoint['model']
-model = model.to(device)
-model.eval()
-
-# Transforms
-resize = transforms.Resize((300, 300))
-to_tensor = transforms.ToTensor()
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-
-rev_label_map = labelMap('data/general_labels/classes.txt')
-print('Classes:\n', rev_label_map)
-label_map = invLabelMap(rev_label_map)
 
 
-distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', 
-                '#911eb4', '#46f0f0', '#f032e6', '#d2f53c', '#fabebe', 
-                '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', 
-                '#aaffc3', '#808000', '#ffd8b1', '#e6beff', '#808080', 
-                '#FFFFFF', '#c6e2ff', '#b0e0e6', '#666a67', '#4beddc', 
-                '#ae415d', '#c1d8c3', '#ffc0cb', '#fb6781', '#f42069',
-                '#ffb6c1', '#102099', '#7c4780', '#a0cf8d', '#f42069',
-                '#fb6781', '#89cff0', '#ffb6c1', '#f42069', '#000000',
-                '#d9534f', '#4a95ff', '#debc97', '#215b8f', '#6fb407', 
-                '#5f8c4a', '#c6ac54', '#8d5d75', '#280050', '#508ef8',
-                '#d109e4', '#534275', '#2761ee', '#15a5ff', '#bd0bad',
-                '#9e0ea1']
-label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+def main():
+    # from cards_class import showBatch
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Load model checkpoint
+    checkpoint = 'data/models/cards.pth.tar'
+    labels_path = 'data/general_labels/classes.txt'
+
+    checkpoint = torch.load(checkpoint)
+    start_epoch = checkpoint['epoch'] + 1
+    model = checkpoint['model']
+    model = model.to(device)
+    model.eval()
 
 
-
-def detect(original_image, min_score, max_overlap, top_k, suppress=None):
-    """
-    Detect objects in an image with a trained SSD300, and visualize the results.
-    :param original_image: image, a PIL Image
-    :param min_score: minimum threshold for a detected box to be considered a match for a certain class
-    :param max_overlap: maximum overlap two boxes can have so that the one with the lower score is not suppressed via Non-Maximum Suppression (NMS)
-    :param top_k: if there are a lot of resulting detection across all classes, keep only the top 'k'
-    :param suppress: classes that you know for sure cannot be in the image or you do not want in the image, a list
-    :return: annotated image, a PIL Image
-    """
-
-    # Transform
-    image = normalize(to_tensor(resize(original_image)))
-
-    # Move to default device
-    image = image.to(device)
-
-    # Forward prop.
-    predicted_locs, predicted_scores = model(image.unsqueeze(0))
-
-    # Detect objects in SSD output
-    det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, 
-                                                            predicted_scores, 
-                                                            min_score=min_score,
-                                                            max_overlap=max_overlap,
-                                                            top_k=top_k)
+    rev_label_map = labelMap(labels_path)
 
 
-    # Move detections to the CPU
-    det_boxes = det_boxes[0].to('cpu')
+    print('Classes:\n', rev_label_map)
+    print('Loaded checkpoint from epoch', start_epoch)
 
-    # Transform to original image dimensions
-    original_dims = torch.FloatTensor([original_image.width, original_image.height, original_image.width, original_image.height]).unsqueeze(0)
-    det_boxes = det_boxes * original_dims
-
-    # Decode class integer labels
-    det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
-
-
-    # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
-    if det_labels == ['background']:
-        w, h = original_image.size
-        if h > 500:
-            fact = 500 / h
-            fact_w = int(fact * w)
-            fact_h = int(fact * h)
-            original_image = transforms.Resize((fact_h, fact_w))(original_image)
-        # Just return original image
-        return original_image
-
-    # Annotate
-    annotated_image = original_image
-    draw = ImageDraw.Draw(annotated_image)
-    try:
-        font = ImageFont.truetype("/usr/share/fonts/truetype/Nakula/nakula.ttf", 100)
-    except OSError:
-        font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial Unicode.ttf", 100)
-
-    # Suppress specific classes, if needed
-    for i in range(det_boxes.size(0)):
-        if suppress is not None:
-            if det_labels[i] in suppress:
-                continue
-
-        # Boxes
-        box_location = det_boxes[i].tolist()
-        draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
-        draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
-        draw.rectangle(xy=[l + 2. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a third rectangle at an offset of 1 pixel to increase line thickness
-        draw.rectangle(xy=[l + 3. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a fourth rectangle at an offset of 1 pixel to increase line thickness
-
-        # Text
-        text_size = font.getsize(det_labels[i].upper())
-        text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
-        textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
-                             box_location[1]]
-        draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
-        draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
-                  font=font)
-    w, h = annotated_image.size
-    if h > 500:
-        fact = 500 / h
-        fact_w = int(fact * w)
-        fact_h = int(fact * h)
-        annotated_image = transforms.Resize((fact_h, fact_w))(annotated_image)
-
-    del draw
-
-    return annotated_image
-
-
-if __name__ == '__main__':
     img_path = 'data/images/Card_492.jpeg'
     original_image = Image.open(img_path, mode='r')
     original_image = original_image.convert('RGB')
-    pred_img = detect(original_image, min_score=0.1, max_overlap=.5, top_k=5).show()
+    pred_img = detect(model=model, rev_label_map=rev_label_map, original_image=original_image, 
+                    min_score=0.1, max_overlap=.5, top_k=5, device=device)
+    show(pred_img)
+
+
+if __name__ == '__main__':
+    main()
     
--- utils.py
+++ utils.py
@@ -81,6 +81,13 @@ def showBatch(batch, factor=1, pred_bbox=None):
         draw.rectangle(rect)
     img.show()
 
+def show(image, factor=1):
+    w, h = image.size
+    fact_w = int(factor * w)
+    fact_h = int(factor * h)
+    img = transforms.Resize((fact_h, fact_w))(image)
+    img.show()
+
 def collate_fn(batch):
     images = list()
     boxes = list()
@@ -151,34 +158,108 @@ def train(train_loader, model, criterion, optimizer, epoch, print_freq=50, grad_
                                                                     data_time=data_time, loss=losses))
     del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
 
-
-# def parse_annotation(annotation_path):
-#     tree = ET.parse(annotation_path)
-#     root = tree.getroot()
-
-#     boxes = list()
-#     labels = list()
+def detect(model, rev_label_map, original_image, min_score, max_overlap, top_k, device, suppress=None):
+    """
+    Detect objects in an image with a trained SSD300, and visualize the results.
+    :param original_image: image, a PIL Image
+    :param min_score: minimum threshold for a detected box to be considered a match for a certain class
+    :param max_overlap: maximum overlap two boxes can have so that the one with the lower score is not suppressed via Non-Maximum Suppression (NMS)
+    :param top_k: if there are a lot of resulting detection across all classes, keep only the top 'k'
+    :param suppress: classes that you know for sure cannot be in the image or you do not want in the image, a list
+    :return: annotated image, a PIL Image
+    """
+    distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0',
+                '#f032e6', '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8',
+                '#800000', '#aaffc3', '#808000', '#ffd8b1', '#e6beff', '#808080',  '#FFFFFF',
+                '#c6e2ff', '#b0e0e6', '#666a67', '#4beddc', '#ae415d', '#c1d8c3', '#ffc0cb',
+                '#fb6781', '#f42069', '#ffb6c1', '#102099', '#7c4780', '#a0cf8d', '#f42069',
+                '#fb6781', '#89cff0', '#ffb6c1', '#f42069', '#000000', '#d9534f', '#4a95ff',
+                '#debc97', '#215b8f', '#6fb407', '#5f8c4a', '#c6ac54', '#8d5d75', '#280050',
+                '#508ef8', '#d109e4', '#534275', '#2761ee', '#15a5ff', '#bd0bad', '#9e0ea1']
     
-#     annotation_path = os.path.join(self.anotation_dir, self.masks[idx])
+    label_map = invLabelMap(rev_label_map)
+    label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+    
+    
 
-#     for object in root.iter('object'):
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    transform = transforms.Compose([
+        transforms.Resize((300, 300)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+        ])
+
+    # Transform
+    image = transform(original_image)
+
+    # Move to default device
+    image = image.to(device)
+
+    # Forward prop.
+    predicted_locs, predicted_scores = model(image.unsqueeze(0))
+
+    # Detect objects in SSD output
+    det_boxes, det_labels, det_scores = model.detect_objects(predicted_locs, 
+                                                            predicted_scores, 
+                                                            min_score=min_score,
+                                                            max_overlap=max_overlap,
+                                                            top_k=top_k)
 
 
-#         label = object.find('name').text.lower().strip()
-#         if label not in label_map:
-#             continue
+    # Move detections to the CPU
+    det_boxes = det_boxes[0].to('cpu')
 
-#         bbox = object.find('bndbox')
-#         xmin = int(bbox.find('xmin').text) - 1
-#         ymin = int(bbox.find('ymin').text) - 1
-#         xmax = int(bbox.find('xmax').text) - 1
-#         ymax = int(bbox.find('ymax').text) - 1
+    # Transform to original image dimensions
+    img_dim = [original_image.width, original_image.height, original_image.width, original_image.height]
+    original_dims = torch.FloatTensor(img_dim).unsqueeze(0)
+    det_boxes = det_boxes * original_dims
 
-#         boxes.append([xmin, ymin, xmax, ymax])
-#         labels.append(label_map[label])
+    # Decode class integer labels
+    det_labels = [rev_label_map[l] for l in det_labels[0].to('cpu').tolist()]
 
 
-#     return {'boxes': boxes, 'labels': labels}
+    # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
+    if det_labels == ['background']:
+        # Just return original image
+        return original_image
+
+    # Annotate
+    annotated_image = original_image
+    draw = ImageDraw.Draw(annotated_image)
+    try:
+        font = ImageFont.truetype("/usr/share/fonts/truetype/Nakula/nakula.ttf", 100)
+    except OSError:
+        font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial Unicode.ttf", 100)
+
+    # Suppress specific classes, if needed
+    for i in range(det_boxes.size(0)):
+        if suppress is not None:
+            if det_labels[i] in suppress:
+                continue
+
+        # Boxes
+        box_location = det_boxes[i].tolist()
+        draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
+        draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
+            det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
+        draw.rectangle(xy=[l + 2. for l in box_location], outline=label_color_map[
+            det_labels[i]])  # a third rectangle at an offset of 1 pixel to increase line thickness
+        draw.rectangle(xy=[l + 3. for l in box_location], outline=label_color_map[
+            det_labels[i]])  # a fourth rectangle at an offset of 1 pixel to increase line thickness
+
+        # Text
+        text_size = font.getsize(det_labels[i].upper())
+        text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
+        textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
+                             box_location[1]]
+        draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
+        draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
+                  font=font)
+    del draw
+
+    return annotated_image
+
 
 
 def create_data_lists(voc07_path, voc12_path, output_folder):
